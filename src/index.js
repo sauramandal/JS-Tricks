22296f3192304308ee8e52117ffb2fc80e29c798
Saura Mandal <saura.mandal1@gmail.com>
2020-08-14 22:49:32 +0530

    Add describeArc & polarToCartesian

--- src/index.js
+++ src/index.js
@@ -273,3 +273,35 @@ const deepEqual = (a, b) => {
 };
 
 console.log(deepEqual(obj1, obj2));
+
+const polarToCartesian = (centerX, centerY, radius, angleInDegrees) => {
+  const angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+  return {
+    x: centerX + radius * Math.cos(angleInRadians),
+    y: centerY + radius * Math.sin(angleInRadians)
+  };
+};
+
+const describeArc = (x, y, radius, startAngle, endAngle) => {
+  const start = polarToCartesian(x, y, radius, endAngle);
+  const end = polarToCartesian(x, y, radius, startAngle);
+
+  const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+  //To draw a specific format in vector graphics
+  return [
+    "M",
+    start.x,
+    start.y,
+    "A",
+    radius,
+    radius,
+    0,
+    largeArcFlag,
+    0,
+    end.x,
+    end.y
+  ].join(" ");
+};
+
+console.log(describeArc(2, 4, 2, 1, 2));
